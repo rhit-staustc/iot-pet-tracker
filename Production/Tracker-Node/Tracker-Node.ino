03b78b4a69e75beea53a26ebdb6b96b0d45d4ac5
rhit-mclemoew <mclemoew@rose-hulman.edu>
2026-05-22 07:42:24 -0400

    finishing touches

--- Production/Tracker-Node/Tracker-Node.ino
+++ Production/Tracker-Node/Tracker-Node.ino
@@ -4,19 +4,12 @@
 #include "LoRaWan_APP.h"
 #include <Wire.h>
 
-// #define DEBUG
-#ifdef DEBUG
-  #define DBG(x)   Serial.print(x)
-  #define DBGLN(x) Serial.println(x)
-#else
-  #define DBG(x)
-  #define DBGLN(x)
-#endif
-
+#define SDA_PIN 45
+#define SCL_PIN 46
 #define PIN_GPS_EN 34
+#define PIN_GPS_RESET 42
 #define GPS_RX_PIN 39
 #define GPS_TX_PIN 38
-#define PIN_GPS_RESET 42
 #define AGE_THRESHOLD 5000
 #define ADC_Ctrl 37
 #define ADC_IN 1
@@ -29,21 +22,19 @@
 #define LORA_SPREADING_FACTOR 10
 #define LORA_CODINGRATE 1
 #define LORA_PREAMBLE_LENGTH 8
-#define LORA_SYMBOL_TIMEOUT 0
 #define LORA_FIX_LENGTH_PAYLOAD_ON false
 #define LORA_IQ_INVERSION_ON false
-#define BUFFER_SIZE 30
 #define BOARD_LED 35
 #define TX_INTERVAL_MS 500
 
-#define RUN_SPEED_IN_MPH 5
-#define WALK_SPEED_IN_MPH 1
-#define HEAD_DIRECTION accelData.accelX
-#define HEAD_ORIENTATION HEAD_DIRECTION
-#define SITTING_THRESHOLD 0.70
-#define SPEED_FILTER_SIZE 8
+#define RUN_SPEED_IN_MPH     5
+#define WALK_SPEED_IN_MPH    1
+#define HEAD_ORIENTATION     accelData.accelZ
+#define SITTING_THRESHOLD    0.70
+#define ROLL_MIN_RATE        50.0
+#define ROLL_ANGLE_THRESHOLD 270.0
 
-uint8_t DEVICE_ID = 0;
+uint8_t DEVICE_ID = 0; // 0 = Bryce, 1 = Ely
 const float VOLT_DIV = 4.9;
 
 TinyGPSPlus gps;
@@ -53,8 +44,10 @@ calData calib = {0};
 AccelData accelData;
 GyroData gyroData;
 
-float speedBuffer[SPEED_FILTER_SIZE] = {0};
-uint8_t speedBufferIdx = 0;
+float rollAccum = 0;
+unsigned long lastRollTime = 0;
+bool rollingDetected = false;
+unsigned long ledFlashUntil = 0;
 
 enum BehaviorState : uint8_t {
   BEHAVIOR_IDLE    = 0,
@@ -63,8 +56,6 @@ enum BehaviorState : uint8_t {
   BEHAVIOR_SITTING = 3,
   BEHAVIOR_ROLLING = 4,
 };
-
-// Pack behavior into the top 4 bits of the status byte: status |= (b << 4)
 
 struct GPSPacket {
   uint8_t status;  // [behavior(4)|fresh(1)|fix_valid(1)|ID(2)]
@@ -75,7 +66,7 @@ struct GPSPacket {
   uint16_t course; // degrees * 100
 };
 
-typedef enum { LOWPOWER, STATE_RX, STATE_TX } States_t;
+typedef enum { LOWPOWER, STATE_TX } States_t;
 
 static RadioEvents_t RadioEvents;
 static unsigned long lastPacket = 0;
@@ -83,7 +74,6 @@ States_t state;
 bool needsBusRecovery = false;
 
 uint8_t getBatteryPercent() {
-  pinMode(ADC_Ctrl, OUTPUT);
   digitalWrite(ADC_Ctrl, HIGH);
   uint32_t vBat = analogReadMilliVolts(ADC_IN) * VOLT_DIV;
   digitalWrite(ADC_Ctrl, LOW);
@@ -91,29 +81,34 @@ uint8_t getBatteryPercent() {
   return (uint8_t)constrain(percent, 0, 100);
 }
 
-double getFilteredSpeed() {
-  speedBuffer[speedBufferIdx] = gps.speed.mph();
-  speedBufferIdx = (speedBufferIdx + 1) % SPEED_FILTER_SIZE;
-
-  double sum = 0;
-  for (int i = 0; i < SPEED_FILTER_SIZE; i++) {
-    sum += speedBuffer[i];
+void updateRolling() {
+  unsigned long now = millis();
+  float dt = (now - lastRollTime) / 1000.0f;
+  lastRollTime = now;
+  float gyroMag = sqrt(sq(gyroData.gyroX) + sq(gyroData.gyroY) + sq(gyroData.gyroZ));
+  if (gyroMag >= ROLL_MIN_RATE) {
+    rollAccum += gyroMag * dt;
+  } else {
+    rollAccum = 0;
   }
-  return sum / SPEED_FILTER_SIZE;
+  if (rollAccum >= ROLL_ANGLE_THRESHOLD) {
+    rollingDetected = true;
+    rollAccum = 0;
+  }
 }
 
 BehaviorState classifyBehavior(double speed_in_mph) {
-  // behavior classification with gps sensor data
   if (speed_in_mph >= RUN_SPEED_IN_MPH) {
     return BEHAVIOR_RUNNING;
   } else if (speed_in_mph >= WALK_SPEED_IN_MPH) {
     return BEHAVIOR_WALKING;
   } else {
-    // behavior subclassification with imu sensor data
-    if (HEAD_ORIENTATION >= SITTING_THRESHOLD) {
+    if (rollingDetected) {
+      rollingDetected = false;
+      return BEHAVIOR_ROLLING;
+    } else if (HEAD_ORIENTATION <= SITTING_THRESHOLD && HEAD_ORIENTATION > 0) {
       return BEHAVIOR_SITTING;
     } else {
-      // TODO: roll-over detection not implemented
       return BEHAVIOR_IDLE;
     }
   }
@@ -124,61 +119,47 @@ GPSPacket buildPacket() {
   bool fixValid = gps.location.isValid();
   bool fresh = gps.location.age() < AGE_THRESHOLD;
   packet.status = (DEVICE_ID & 0x03);
-
-  if (fixValid)
-    packet.status |= (1 << 2);
-  if (fresh)
-    packet.status |= (1 << 3);
-
-  double filteredSpeed = getFilteredSpeed();
-  packet.status |= (classifyBehavior(filteredSpeed) << 4);
+  if (fixValid) packet.status |= (1 << 2);
+  if (fresh)    packet.status |= (1 << 3);
+  double speed = gps.speed.mph();
+  packet.status |= (classifyBehavior(speed) << 4);
   packet.battery = getBatteryPercent();
   packet.lat = (float)gps.location.lat();
   packet.lon = (float)gps.location.lng();
-  packet.speed = (uint16_t)(filteredSpeed * 100);
+  packet.speed = (uint16_t)(speed * 100);
   packet.course = (uint16_t)(gps.course.deg() * 100);
   return packet;
 }
 
-const char* behaviorName(uint8_t status) {
-  switch ((BehaviorState)(status >> 4)) {
-    case BEHAVIOR_WALKING: return "Walking";
-    case BEHAVIOR_RUNNING: return "Running";
-    case BEHAVIOR_SITTING: return "Sitting";
-    case BEHAVIOR_ROLLING: return "Rolling";
-    default:               return "Idle";
-  }
+void OnTxDone(void) {
+  ledFlashUntil = millis() + 80;
+  digitalWrite(BOARD_LED, HIGH);
+  needsBusRecovery = true;
+  state = LOWPOWER;
 }
 
-void printPacket(GPSPacket &packet) {
-  Serial.print("ID: ");
-  Serial.print(packet.status & 0x03);
-  Serial.print(" | Fix: ");
-  Serial.print((packet.status >> 2) & 1 ? "YES" : "NO");
-  Serial.print(" | Fresh: ");
-  Serial.print((packet.status >> 3) & 1 ? "YES" : "NO");
-  Serial.print(" | Behavior: ");
-  Serial.print(behaviorName(packet.status));
-  Serial.print(" | Batt: ");
-  Serial.print(packet.battery);
-  Serial.print("% | Lat: ");
-  Serial.print(packet.lat, 6);
-  Serial.print(" | Lon: ");
-  Serial.print(packet.lon, 6);
-  Serial.print(" | Speed: ");
-  Serial.print(packet.speed / 100.0);
-  Serial.print("mph | Dir: ");
-  Serial.println(packet.course / 100.0);
+void OnTxTimeout(void) {
+  Radio.Sleep();
+  needsBusRecovery = true;
+  state = STATE_TX;
+}
+
+void gpsConfig() {
+  Serial1.println("$PCAS04,7*1E");
+  Serial1.println("$PCAS11,1*1C");
+  Serial1.println("$PCAS02,1000*2E");
+  Serial1.println("$PCAS03,0,0,0,0,1,0,0,0,0,0,,,0,0*03");
+  Serial1.println("$PCAS00*01");
 }
 
 void setup() {
-  Serial.begin(115200);
-
   Mcu.begin(HELTEC_BOARD, SLOW_CLK_TPYE);
-  Serial.println("Mcu initialized");
 
-  Wire1.begin(6, 7);
+  Wire1.begin(SDA_PIN, SCL_PIN);
   Wire1.setClock(400000);
+
+  pinMode(PIN_GPS_RESET, OUTPUT);
+  digitalWrite(PIN_GPS_RESET, HIGH);
 
   pinMode(PIN_GPS_EN, OUTPUT);
   digitalWrite(PIN_GPS_EN, LOW);
@@ -186,30 +167,21 @@ void setup() {
   Serial1.begin(9600, SERIAL_8N1, GPS_RX_PIN, GPS_TX_PIN);
   gpsConfig();
 
-  Serial.println("Initializing IMU...");
+  calib.valid = true;
+  calib.accelBias[0] =  0.02;
+  calib.accelBias[1] =  0.03;
+  calib.accelBias[2] = -0.01;
+  calib.gyroBias[0]  =  3.06;
+  calib.gyroBias[1]  =  0.45;
+  calib.gyroBias[2]  = -0.45;
   IMU.init(calib, 0x68);
-  Serial.println("IMU init complete. Keep IMU level.");
-  delay(5000);
-  Serial.println("Starting IMU calibration...");
-  IMU.calibrateAccelGyro(&calib);
-  IMU.init(calib, 0x68);  // re-init to apply biases and restore ±2000dps range
-  Serial.println("Calibration done!");
-  DBG("Accel biases X/Y/Z: ");
-  DBG(calib.accelBias[0]); DBG(", ");
-  DBG(calib.accelBias[1]); DBG(", ");
-  DBGLN(calib.accelBias[2]);
-  DBG("Gyro biases X/Y/Z: ");
-  DBG(calib.gyroBias[0]); DBG(", ");
-  DBG(calib.gyroBias[1]); DBG(", ");
-  DBGLN(calib.gyroBias[2]);
-  delay(2000);
 
+  pinMode(ADC_Ctrl, OUTPUT);
   pinMode(BOARD_LED, OUTPUT);
   digitalWrite(BOARD_LED, LOW);
 
   RadioEvents.TxDone = OnTxDone;
   RadioEvents.TxTimeout = OnTxTimeout;
-  RadioEvents.RxDone = OnRxDone;
 
   Radio.Init(&RadioEvents);
   Radio.SetChannel(RF_FREQUENCY);
@@ -217,83 +189,40 @@ void setup() {
                     LORA_SPREADING_FACTOR, LORA_CODINGRATE,
                     LORA_PREAMBLE_LENGTH, LORA_FIX_LENGTH_PAYLOAD_ON, true, 0,
                     0, LORA_IQ_INVERSION_ON, 3000);
-  Radio.SetRxConfig(MODEM_LORA, LORA_BANDWIDTH, LORA_SPREADING_FACTOR,
-                    LORA_CODINGRATE, 0, LORA_PREAMBLE_LENGTH,
-                    LORA_SYMBOL_TIMEOUT, LORA_FIX_LENGTH_PAYLOAD_ON, 0, true, 0,
-                    0, LORA_IQ_INVERSION_ON, true);
 
   state = STATE_TX;
-}
-
-void gpsConfig() {
-  Serial.print("Device ID: ");
-  Serial.println(DEVICE_ID);
-  Serial.println("GPS starting...");
-  Serial1.println("$PCAS04,7*1E");                         // all constellations
-  Serial1.println("$PCAS11,1*1C");                         // pedestrian mode
-  Serial1.println("$PCAS02,1000*2E");                      // 1hz
-  Serial1.println("$PCAS03,0,0,0,0,1,0,0,0,0,0,,,0,0*03"); // RMC mode
-  Serial1.println("$PCAS00*01");                           // save to flash
-  Serial.println("GPS configured and saved!");
-}
-
-void OnTxDone(void) {
-  DBGLN("TX done");
-  needsBusRecovery = true;
-  state = STATE_RX;
-}
-
-void OnTxTimeout(void) {
-  Radio.Sleep();
-  DBGLN("TX Timeout");
-  needsBusRecovery = true;
-  state = STATE_TX;
-}
-
-void OnRxDone(uint8_t *payload, uint16_t size, int16_t rssi, int8_t snr) {
-  Radio.Sleep();
 }
 
 void loop() {
   switch (state) {
   case STATE_TX: {
-    while (Serial1.available()) {
-      gps.encode(Serial1.read());
-    }
+    while (Serial1.available()) gps.encode(Serial1.read());
     if (needsBusRecovery) {
       Wire1.end();
-      Wire1.begin(6, 7);
+      Wire1.begin(SDA_PIN, SCL_PIN);
       Wire1.setClock(400000);
-      IMU.init(calib, 0x68);
       needsBusRecovery = false;
     }
     IMU.update();
     IMU.getAccel(&accelData);
     IMU.getGyro(&gyroData);
-    DBG("Accel X/Y/Z: "); DBG(accelData.accelX); DBG(", "); DBG(accelData.accelY); DBG(", "); DBGLN(accelData.accelZ);
-    DBG("HeadOrient: "); DBG(HEAD_ORIENTATION); DBG(" (threshold: "); DBG(SITTING_THRESHOLD); DBGLN(")");
+    updateRolling();
     GPSPacket packet = buildPacket();
-    printPacket(packet);
-    digitalWrite(BOARD_LED, HIGH);
     Radio.Send((uint8_t *)&packet, sizeof(GPSPacket));
-    digitalWrite(BOARD_LED, LOW);
     lastPacket = millis();
     state = LOWPOWER;
     break;
   }
-  case STATE_RX:
-    DBGLN("into RX mode");
-    Radio.Rx(0);
-    lastPacket = millis();
-    state = LOWPOWER;
-    break;
   case LOWPOWER:
-    while (Serial1.available()) {
-      gps.encode(Serial1.read());
-    }
+    while (Serial1.available()) gps.encode(Serial1.read());
     IMU.update();
     IMU.getAccel(&accelData);
     IMU.getGyro(&gyroData);
+    updateRolling();
+    if (ledFlashUntil && millis() > ledFlashUntil) {
+      digitalWrite(BOARD_LED, LOW);
+      ledFlashUntil = 0;
+    }
     Radio.IrqProcess();
     if (millis() - lastPacket >= TX_INTERVAL_MS) {
       Radio.Sleep();
